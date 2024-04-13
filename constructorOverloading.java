fcb221b850fc4f303a85421f4457f2b8fbd99468
KISHLAY568 <kishlayk568@gmail.com>
2024-04-13 10:30:08 +0530

    first commit

---
+++ constructorOverloading.java
@@ -0,0 +1,9 @@
+public class constructorOverloading {
+    public static void main(String[] args) {
+        Student s1 = new Student("Kishlay", 21);
+        s1.disp();
+        Student s2 = new Student("Ranjan", 24);
+        s2.disp();
+    }
+
+}
