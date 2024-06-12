1e84548006c074d3cd22c490f8d31d8d34748c5a
KISHLAY568 <kishlayk568@gmail.com>
2024-06-12 15:16:30 +0530

    first commit

---
+++ SumArray.java
@@ -0,0 +1,11 @@
+public class SumArray {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6,7,8,9,0};
+        int sum = 0;
+        for(int n:a)
+        {
+            sum = sum + n;
+        }
+        System.out.println(sum);
+    }
+}
