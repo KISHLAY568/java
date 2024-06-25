26ca9e284faf31c4fea01e34880e9514a87a1aed
KISHLAY568 <kishlayk568@gmail.com>
2024-06-25 15:27:11 +0530

    first commit

---
+++ PrintArray.java
@@ -0,0 +1,11 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6,7,8,9,0};
+        
+        for(int n:a)
+        {
+            System.out.print(n+" ");
+        }
+    }
+    
+}
